924f372acebd0f2824b98f5d75a7502d5e8e0d8d
naylaakas <naylakas05@gmail.com>
2024-10-04 10:36:07 +0700

    modif pertanyaan

--- Pemilihan2Percobaan121.java
+++ Pemilihan2Percobaan121.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Pemilihan2Percobaan121 {
-
+ 
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         int tahun;
@@ -8,9 +8,17 @@ public class Pemilihan2Percobaan121 {
         System.out.print("Masukkan Tahun: ");
         tahun = input21.nextInt();
 
-        if ((tahun % 4 == 0) && ((tahun % 100 != 0))) {
-            System.out.println("Tahun Kabisat");
-    }  else
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0 ) {    
+                System.out.println("Tahun Kabisat");
+            }  else {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
+            }
+     } else
         System.out.println("Bukan Tahun Kabisat");  
     }
 }
